ad9964d2fff8acaf29a769b1372c63495c53509a
Özgür İnce <ozgur@ince.ist>
2020-03-31 20:18:40 +0300

    modify 48

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private float _speed = 4f;
+
+    void Update()
+    {
+        transform.Translate(Vector3.down * _speed * Time.deltaTime);
+        if (transform.position.y < -7f)
+            transform.position = new Vector3(UnityEngine.Random.Range(-10f, 10f), 7f, 0);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Laser"))
+            Destroy(other.gameObject);
+        else if (other.CompareTag("Player"))
+            DamagePlayer(other);
+        Destroy(gameObject);
+    }
+
+    private void DamagePlayer(Collider other)
+    {
+        var player = other.gameObject.GetComponent<Player>();
+        if (player != null)
+            player.Damage();
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,9 +2,10 @@
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private float _speed = 3.5f;
+    [SerializeField] private float _speed = 10f;
     [SerializeField] private GameObject laserPrefab;
     [SerializeField] private float _fireRate = 0.15f;
+    [SerializeField] private int _lives = 3;
 
     private float _nextFire;
 
@@ -33,5 +34,11 @@ public class Player : MonoBehaviour
         var vertical = Input.GetAxis("Vertical");
         transform.Translate((Vector3.right * horizontal + Vector3.up * vertical) * _speed * Time.deltaTime);
         transform.position = new Vector3(transform.position.x > 11.3f ? -11.3f : transform.position.x < -11.3f ? 11.3f : transform.position.x, Mathf.Clamp(transform.position.y, -3.8f, 0f), 0f);
+    }
+
+    public void Damage()
+    {
+        if (--_lives < 1)
+            Destroy(gameObject);
     }
 }
